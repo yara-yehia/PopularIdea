06694bfc462c849a9f9d31a14f420f7977cb0da4
yara-yehia <yehiayara116@gmail.com>
2020-05-29 00:47:50 +0200

    Add database For ContactUS

--- database/migrations/2020_03_03_150746_create_posts_table.php
+++ database/migrations/2020_03_03_150746_create_posts_table.php
@@ -17,6 +17,7 @@ class CreatePostsTable extends Migration
             $table->id();
             $table->string('title');
             $table->mediumText('body');
+            $table->string('cover_image');
             $table->timestamps();
         });
     }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,8 +11,10 @@
             {{Form::label('body', 'Body')}}
             {{Form::textarea('body', '', ['class' => 'form-control', 'placeholder' => 'Body Text'])}}
         </div>
-        
+
+
+
         {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
     {!! Form::close() !!}
-    
+
 @endsection
