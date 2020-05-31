10f433e1b4e614c9218ee3570358692688814ff7
nohaabdo98 <62175401+nohaabdo98@users.noreply.github.com>
2020-05-31 15:45:57 +0200

    Update navbar for add contact us

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1>Edit Post</h1>
-    {!! Form::open(['action' => ['PostController@update', $post->id], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+    {!! Form::open(['action' => ['PostController@update', $post->id], 'method' => 'POST']) !!}
         <div class="form-group">
             {{Form::label('title', 'Title')}}
             {{Form::text('title', $post->title, ['class' => 'form-control', 'placeholder' => 'Title'])}}
@@ -11,9 +11,7 @@
             {{Form::label('body', 'Body')}}
             {{Form::textarea('body', $post->body, ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Body Text'])}}
         </div>
-        <div class="form-group">
-            {{Form::file('cover_image')}}
-        </div>
+
         {{Form::hidden('_method','PUT')}}
         {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
     {!! Form::close() !!}
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,12 +2,20 @@
 
 @section('content')
     <h1>Posts</h1>
+
     @if (count($posts) > 0)
         @foreach ($posts as $post)
             <div class="well">
-            <h3>    <a href="/posts/{{$post->id}}"> {{$post->title}} </a>    </h3>
-            <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
-            </div>  
+                <div class="row">
+
+                    <div class="col-md-8 col-sm-8">
+                        <h3>    <a href="/posts/{{$post->id}}"> {{$post->title}} </a>    </h3>
+
+                        <br>
+                        <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
+                    </div>
+                </div>
+            </div>
         @endforeach
         {{$posts->links()}}
 
