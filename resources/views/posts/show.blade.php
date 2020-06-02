01e78696c0830922146a11cf7d5faac0aa0385b8
nohaabdo98 <62175401+nohaabdo98@users.noreply.github.com>
2020-06-02 16:46:36 +0200

    add plugin comment

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,24 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v7.0&appId=871772069994112&autoLogAppEvents=1"></script>
     <a href="/posts" class="btn btn-default">Go Back</a>
-    <h1>{{$post->title}}</h1>
-    <img style="width:100%" src="/storage/cover_images/{{$post->cover_image}}">
-    <br><br>
-    <div>
-        {!!$post->body!!}
-    </div>
-    <hr>
-    <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
-    <hr>
-    @if(!Auth::guest())
-        @if(Auth::user()->id == $post->user_id)
-            <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+    <div class="well">
+        <div class="row">
+            <h3 style="text-align: center">{{$post->title}}</h3>
+        </div>
+        <hr>
+        <div class="row">
+            <h3>Post Body<br></h3> {!!$post->body!!}
+        </div>
+        <hr>
+        <div class="row">
+            <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
+        </div>
 
-            {!!Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
-                {{Form::hidden('_method', 'DELETE')}}
-                {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-            {!!Form::close()!!}
+        @if(!Auth::guest())
+            @if(Auth::user()->id == $post->user_id)
+                <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+
+                {!!Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                    {{Form::hidden('_method', 'DELETE')}}
+                    {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                {!!Form::close()!!}
+            @endif
         @endif
-    @endif
+    </div>
+    <div class="fb-comments" data-href="https://localhost:8000" data-numposts="5" data-width=""></div>
+
 @endsection
